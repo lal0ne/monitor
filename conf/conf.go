f86e25818f0c001faa2b0758c1ce2157ba721d3b
la10ne <22907374+lal0ne@users.noreply.github.com>
2023-04-19 14:54:46 +0800

    :hammer: 更改使用viper

--- conf/conf.go
+++ conf/conf.go
@@ -2,8 +2,9 @@ package conf
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v2"
-	"io/ioutil"
+	"github.com/spf13/viper"
+	//"gopkg.in/yaml.v2"
+	//"io/ioutil"
 	"os"
 )
 
@@ -15,20 +16,43 @@ func init() {
 		}
 	}()
 
-	// 打开配置文件
-	file, err := os.Open("conf.yaml")
+	/*
+		yaml版本
+		// 打开配置文件
+		file, err := os.Open("conf.yaml")
+		if err != nil {
+			panic(err)
+		}
+
+		// 读取数据
+		bytes, err := ioutil.ReadAll(file)
+		if err != nil {
+			panic(err)
+		}
+
+		// 配置与结构体做关系映射
+		err = yaml.Unmarshal(bytes, &CFG)
+		if err != nil {
+			panic(err)
+		}
+	*/
+
+	// 获取项目路径
+	path, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
 
-	// 读取数据
-	bytes, err := ioutil.ReadAll(file)
-	if err != nil {
+	// 读取配置
+	vip := viper.New()
+	vip.AddConfigPath(path)
+	vip.SetConfigName("conf")
+	vip.SetConfigType("yaml")
+	// 读取配置
+	if err := vip.ReadInConfig(); err != nil {
 		panic(err)
 	}
-
-	// 配置与结构体做关系映射
-	err = yaml.Unmarshal(bytes, &CFG)
+	err = vip.Unmarshal(&CFG)
 	if err != nil {
 		panic(err)
 	}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module monitor
 go 1.16
 
 require (
-	github.com/fsnotify/fsnotify v1.5.1
+	github.com/fsnotify/fsnotify v1.6.0
+	github.com/spf13/viper v1.15.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0
 )
